64f900b4cb7bbdeec75b9757fd8da72436900719
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-01-18 13:16:05 +0330

    Update Uploader.cs

--- Uploader.cs
+++ Uploader.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+// Copyright © 2023 Saeed Molaiy. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Http;
 
 namespace FileUploader
 {
